bsdimp/freebsd-ci

commit d5aabc1ceb01e73c387da4bf3657b85267151d7a

gmirror: Fix a bug introduced in r341674
r341674 inadvertently introduced a bug where newer mirror components being
tasted would clear the high sc_flags that are not controlled by component
metadata, such as G_MIRROR_DEVICE_FLAG_TASTING.  This could plausibly expose
a small window of time during STARTING where device destruction might race
with mirror component addition, probably resulting in a crash.

Reviewed by:	markj
X-MFC-With:	r341674
Differential Revision:	https://reviews.freebsd.org/D18521


git-svn-id: svn+ssh://svn.freebsd.org/base/head@341840 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/geom/mirror/g_mirror.c
@@ -3061,6 +3061,8 @@ g_mirror_reinit_from_metadata(struct g_mirror_softc *sc,
     const struct g_mirror_metadata *md)
 {
 
+	sx_assert(&sc->sc_lock, SX_XLOCKED);
+
 	sc->sc_genid = md->md_genid;
 	sc->sc_syncid = md->md_syncid;
 
@@ -3068,7 +3070,8 @@ g_mirror_reinit_from_metadata(struct g_mirror_softc *sc,
 	sc->sc_balance = md->md_balance;
 	sc->sc_mediasize = md->md_mediasize;
 	sc->sc_ndisks = md->md_all;
-	sc->sc_flags = md->md_mflags;
+	sc->sc_flags &= ~G_MIRROR_DEVICE_FLAG_MASK;
+	sc->sc_flags |= (md->md_mflags & G_MIRROR_DEVICE_FLAG_MASK);
 }
 
 struct g_geom *

commit 103a4d5bdb7a403c1fa045ae78e47ad5d794549e

gmirror: Remove a last-minute INVARIANTS breakage in r341840
I mistakenly added a lock assertion to this routine at the last minute
without confirming it was held during g_mirror_create.  It isn't (it isn't
even initialized yet).  Mea culpa.  Access is exclusive in both callers,
just not always by that particular lock.

Reported by:	lwhsu
X-MFC-With:	r341840, r341674


git-svn-id: svn+ssh://svn.freebsd.org/base/head@341994 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/geom/mirror/g_mirror.c
@@ -3061,8 +3061,6 @@ g_mirror_reinit_from_metadata(struct g_mirror_softc *sc,
     const struct g_mirror_metadata *md)
 {
 
-	sx_assert(&sc->sc_lock, SX_XLOCKED);
-
 	sc->sc_genid = md->md_genid;
 	sc->sc_syncid = md->md_syncid;
 