a5da596e67542b4564d6bb139ca9d2acca634866
M. Warner Losh <wlosh@netflix.com>
2020-02-23 23:36:34 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/allwinner/aw_syscon.c
+++ sys/arm/allwinner/aw_syscon.c
@@ -61,8 +61,6 @@ static int
 aw_syscon_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
