e8debbdfea6e25760a7eee47840a48833cb3d58f
M. Warner Losh <wlosh@netflix.com>
2020-02-23 23:36:28 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/allwinner/aw_cir.c
+++ sys/arm/allwinner/aw_cir.c
@@ -379,9 +379,6 @@ static int
 aw_ir_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
