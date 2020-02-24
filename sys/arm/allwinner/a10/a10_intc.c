f8339054dfd438015017e73c0441ca2bd0000b12
M. Warner Losh <wlosh@netflix.com>
2020-02-23 23:36:24 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/allwinner/a10/a10_intc.c
+++ sys/arm/allwinner/a10/a10_intc.c
@@ -311,9 +311,6 @@ static int
 a10_aintc_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "allwinner,sun4i-a10-ic"))
 		return (ENXIO);
 	device_set_desc(dev, "A10 AINTC Interrupt Controller");
