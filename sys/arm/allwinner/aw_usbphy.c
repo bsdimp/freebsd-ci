4a245f90b1f09f11e2faae8fa89b1fe4a85a279d
M. Warner Losh <wlosh@netflix.com>
2020-02-23 23:36:36 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/allwinner/aw_usbphy.c
+++ sys/arm/allwinner/aw_usbphy.c
@@ -440,9 +440,6 @@ out:
 static int
 awusbphy_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
