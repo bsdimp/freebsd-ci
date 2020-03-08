7a14b6d5fae52afb13e701c114c0e19b651f8c94
M. Warner Losh <wlosh@netflix.com>
2020-03-07 20:03:35 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/altera/socfpga/socfpga_manager.c
+++ sys/arm/altera/socfpga/socfpga_manager.c
@@ -374,9 +374,6 @@ static int
 fpgamgr_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "altr,socfpga-fpga-mgr"))
 		return (ENXIO);
 
