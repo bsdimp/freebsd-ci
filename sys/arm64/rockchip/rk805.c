84f990180c50077573559c9e39ed07d7d7439490
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:45 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm64/rockchip/rk805.c
+++ sys/arm64/rockchip/rk805.c
@@ -571,8 +571,6 @@ rk805_reg_attach(device_t dev, phandle_t node,
 static int
 rk805_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
