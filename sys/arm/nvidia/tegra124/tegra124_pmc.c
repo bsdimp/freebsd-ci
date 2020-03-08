f741b7e6fe884d3a258697006e10ce718a3e3756
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:04 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/nvidia/tegra124/tegra124_pmc.c
+++ sys/arm/nvidia/tegra124/tegra124_pmc.c
@@ -459,9 +459,6 @@ static int
 tegra124_pmc_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
