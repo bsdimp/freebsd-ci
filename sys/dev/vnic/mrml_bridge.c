47b5759c7b0a7567ac1a4499609eaf104aeb4df8
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:31 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/vnic/mrml_bridge.c
+++ sys/dev/vnic/mrml_bridge.c
@@ -94,9 +94,6 @@ static int
 mrmlb_fdt_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "cavium,thunder-8890-mrml-bridge"))
 		return (ENXIO);
 
