ee017a8d7f1885db023f343d9cccf0ff70bccffc
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:09 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/iicbus/iicoc_fdt.c
+++ sys/dev/iicbus/iicoc_fdt.c
@@ -144,9 +144,6 @@ static int
 iicoc_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
