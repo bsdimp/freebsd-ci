ce8a90d8079e90443cadc39ff1fa26c74987864e
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:21 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/ti/am335x/am335x_gpio.c
+++ sys/arm/ti/am335x/am335x_gpio.c
@@ -66,9 +66,6 @@ static int
 am335x_gpio_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
