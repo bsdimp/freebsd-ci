49baf9b528a46c26344a5d68066332e07d43ef18
M. Warner Losh <wlosh@netflix.com>
2020-03-07 20:07:26 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/amlogic/aml8726/aml8726_rtc.c
+++ sys/arm/amlogic/aml8726/aml8726_rtc.c
@@ -358,9 +358,6 @@ static int
 aml8726_rtc_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "amlogic,aml8726-rtc"))
 		return (ENXIO);
 
