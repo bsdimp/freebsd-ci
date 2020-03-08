dcdc5727a76c2b567d94d547e63cc296a116bfb6
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:02 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/nvidia/as3722.c
+++ sys/arm/nvidia/as3722.c
@@ -271,9 +271,6 @@ static int
 as3722_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
