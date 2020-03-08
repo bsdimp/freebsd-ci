26ae28c935a953be8fd99399aa1fc66e16af9813
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:38 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/freescale/imx/imx6_ipu.c
+++ sys/arm/freescale/imx/imx6_ipu.c
@@ -1104,9 +1104,6 @@ ipu_probe(device_t dev)
 	if (have_ipu)
 		return (ENXIO);
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "fsl,imx6q-ipu"))
 		return (ENXIO);
 
