5996537fe272067776e671365d3945336b8047e4
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:56 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/mv/mv_ap806_sei.c
+++ sys/arm/mv/mv_ap806_sei.c
@@ -295,9 +295,6 @@ static int
 mv_ap806_sei_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
