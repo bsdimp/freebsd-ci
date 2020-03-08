0879e2652a9ca5534e33e9a51858c9e5f9d895ca
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:46 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm64/rockchip/rk_gpio.c
+++ sys/arm64/rockchip/rk_gpio.c
@@ -110,9 +110,6 @@ static int
 rk_gpio_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
