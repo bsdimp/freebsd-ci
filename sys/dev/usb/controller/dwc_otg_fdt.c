445788d4e763b80d0ebcd05a852869f9531d38bc
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:26 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/usb/controller/dwc_otg_fdt.c
+++ sys/dev/usb/controller/dwc_otg_fdt.c
@@ -69,9 +69,6 @@ static int
 dwc_otg_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
