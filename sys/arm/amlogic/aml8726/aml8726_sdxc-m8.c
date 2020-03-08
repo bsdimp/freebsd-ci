d7b2f3545eb58d25534bcd3e6f9fa1a44bd6a41e
M. Warner Losh <wlosh@netflix.com>
2020-03-07 20:07:26 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/amlogic/aml8726/aml8726_sdxc-m8.c
+++ sys/arm/amlogic/aml8726/aml8726_sdxc-m8.c
@@ -683,9 +683,6 @@ static int
 aml8726_sdxc_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "amlogic,aml8726-sdxc-m8"))
 		return (ENXIO);
 
