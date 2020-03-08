835abb6fa9cdc895840c65d667c32dd1bbbc5712
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:24 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/uart/uart_bus_fdt.c
+++ sys/dev/uart/uart_bus_fdt.c
@@ -261,10 +261,6 @@ uart_fdt_probe(device_t dev)
 	int err;
 
 	sc = device_get_softc(dev);
-
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	sc->sc_class = (struct uart_class *)uart_fdt_find_device(dev);
 	if (sc->sc_class == NULL)
 		return (ENXIO);
