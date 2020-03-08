336609d8e36beb09782deb0177b03d003becde35
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:58 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/beri/virtio/virtio_mmio_platform.c
+++ sys/dev/beri/virtio/virtio_mmio_platform.c
@@ -114,9 +114,6 @@ static int
 virtio_mmio_platform_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "beri,virtio_mmio_platform"))
 		return (ENXIO);
 
