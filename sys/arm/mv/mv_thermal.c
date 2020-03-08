b6b46137e092e297116a1c5eaf139f48f61a1f42
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:59 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/mv/mv_thermal.c
+++ sys/arm/mv/mv_thermal.c
@@ -282,9 +282,6 @@ static int
 mv_thermal_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
