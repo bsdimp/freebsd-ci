f073415db1c947b9a302fd6ff67196a15a5a22b1
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:55 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/mv/mv_ap806_clock.c
+++ sys/arm/mv/mv_ap806_clock.c
@@ -112,9 +112,6 @@ static int
 mv_ap806_clock_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
