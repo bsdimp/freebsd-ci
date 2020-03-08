c4a10e49de820009deaa00c38198947a9cff6bfc
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:42 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm64/intel/stratix10-soc-fpga-mgr.c
+++ sys/arm64/intel/stratix10-soc-fpga-mgr.c
@@ -206,9 +206,6 @@ static int
 fpgamgr_s10_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "intel,stratix10-soc-fpga-mgr"))
 		return (ENXIO);
 
