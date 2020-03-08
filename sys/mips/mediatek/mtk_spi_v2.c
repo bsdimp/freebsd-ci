68ec650e0603388776990e0f68e4c8f22c46169f
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:50 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/mediatek/mtk_spi_v2.c
+++ sys/mips/mediatek/mtk_spi_v2.c
@@ -104,9 +104,6 @@ static int
 mtk_spi_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return(ENXIO);
 
