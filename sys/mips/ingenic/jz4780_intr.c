63ce22b7b93083d02af55b35bb5ebee04049e275
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:39 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/ingenic/jz4780_intr.c
+++ sys/mips/ingenic/jz4780_intr.c
@@ -95,9 +95,6 @@ static int
 jz4780_pic_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 	device_set_desc(dev, "JZ4780 Interrupt Controller");
