ece5e091e3de4fd7e63469acc07b7f6e594a58d5
M. Warner Losh <wlosh@netflix.com>
2020-03-07 20:07:24 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/amlogic/aml8726/aml8726_pic.c
+++ sys/arm/amlogic/aml8726/aml8726_pic.c
@@ -131,9 +131,6 @@ static int
 aml8726_pic_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "amlogic,aml8726-pic"))
 		return (ENXIO);
 
