3d0da8e19ab0be0a6a25fe4e66063a20c6f733a3
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:17 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/neta/if_mvneta_fdt.c
+++ sys/dev/neta/if_mvneta_fdt.c
@@ -93,9 +93,6 @@ static int
 mvneta_fdt_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
