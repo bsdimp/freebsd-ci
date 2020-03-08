7f14eed674b327d53e0fb081859bec76cbe4d6e2
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:30 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/ti/ti_sysc.c
+++ sys/arm/ti/ti_sysc.c
@@ -64,8 +64,6 @@ static int ti_sysc_detach(device_t dev);
 static int
 ti_sysc_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
