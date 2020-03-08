b76e9b8f16b390ba9edba8e810bbb907197c90af
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:51 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/mv/a37x0_iic.c
+++ sys/arm/mv/a37x0_iic.c
@@ -171,9 +171,6 @@ static int
 a37x0_iic_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
