b555f7d3c8ef11895153574785b7642b87b78f61
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:18 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/samsung/exynos/exynos5_usb_phy.c
+++ sys/arm/samsung/exynos/exynos5_usb_phy.c
@@ -131,9 +131,6 @@ static int
 usb_phy_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "samsung,exynos5420-usbdrd-phy"))
 		return (ENXIO);
 
