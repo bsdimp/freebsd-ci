ab24f484fb712feb86aa4b862292769cc68b9d78
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:04 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/nvidia/tegra124/tegra124_xusbpadctl.c
+++ sys/arm/nvidia/tegra124/tegra124_xusbpadctl.c
@@ -1124,9 +1124,6 @@ static int
 xusbpadctl_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
