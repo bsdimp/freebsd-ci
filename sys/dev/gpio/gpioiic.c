64474be143f338d2965aa209e96156185feb8750
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:05 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/gpio/gpioiic.c
+++ sys/dev/gpio/gpioiic.c
@@ -273,8 +273,7 @@ gpioiic_probe(device_t dev)
 	rv = BUS_PROBE_NOWILDCARD;
 
 #ifdef FDT
-	if (ofw_bus_status_okay(dev) &&
-	    ofw_bus_search_compatible(dev, compat_data)->ocd_data)
+	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data)
                 rv = BUS_PROBE_DEFAULT;
 #endif
 
