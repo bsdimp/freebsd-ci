ae8a8e8b14f43107c16da3cb16a71b5be1673e40
M. Warner Losh <wlosh@netflix.com>
2020-03-07 20:07:25 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/amlogic/aml8726/aml8726_pinctrl.c
+++ sys/arm/amlogic/aml8726/aml8726_pinctrl.c
@@ -102,9 +102,6 @@ static int
 aml8726_pinctrl_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "amlogic,aml8726-pinctrl"))
 		return (ENXIO);
 
