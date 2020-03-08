df0032927199ac367bcdb36dbe594f1db44b2f63
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:20 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/ti/am335x/am335x_ehrpwm.c
+++ sys/arm/ti/am335x/am335x_ehrpwm.c
@@ -553,9 +553,6 @@ static int
 am335x_ehrpwm_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
