e28f98c3863c3df4a0423c08f737ce3803e240f3
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:40 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/freescale/imx/imx6_usbphy.c
+++ sys/arm/freescale/imx/imx6_usbphy.c
@@ -172,9 +172,6 @@ static int
 usbphy_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
