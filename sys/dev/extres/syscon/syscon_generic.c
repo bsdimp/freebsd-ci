78ac6b24afd87a1390b0a4cc5d60b49c4774e0f3
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:01 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/extres/syscon/syscon_generic.c
+++ sys/dev/extres/syscon/syscon_generic.c
@@ -136,8 +136,6 @@ static int
 syscon_generic_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
