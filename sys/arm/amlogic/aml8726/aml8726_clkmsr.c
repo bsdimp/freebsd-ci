b0b8e874da5624c04d964e6f5c06c5bca0776166
M. Warner Losh <wlosh@netflix.com>
2020-03-07 20:07:22 -0700

    Remove ifw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/amlogic/aml8726/aml8726_clkmsr.c
+++ sys/arm/amlogic/aml8726/aml8726_clkmsr.c
@@ -175,9 +175,6 @@ static int
 aml8726_clkmsr_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "amlogic,aml8726-clkmsr"))
 		return (ENXIO);
 
