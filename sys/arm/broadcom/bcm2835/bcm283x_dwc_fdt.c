390c27e2da520ba694da9caaba5e77e51d2442ea
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:34 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/broadcom/bcm2835/bcm283x_dwc_fdt.c
+++ sys/arm/broadcom/bcm2835/bcm283x_dwc_fdt.c
@@ -65,9 +65,6 @@ static int
 bcm283x_dwc_otg_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
