1baf1833ec964d0a06e0ada9a5fe6197272d742a
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:41 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/ingenic/jz4780_nemc.c
+++ sys/mips/ingenic/jz4780_nemc.c
@@ -84,9 +84,6 @@ static int
 jz4780_nemc_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "ingenic,jz4780-nemc"))
 		return (ENXIO);
 
