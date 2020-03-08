8c978bfeb89697f143faa9a9366d0dd921136482
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:28 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/usb/controller/ehci_mv.c
+++ sys/dev/usb/controller/ehci_mv.c
@@ -132,9 +132,6 @@ static int
 mv_ehci_probe(device_t self)
 {
 
-	if (!ofw_bus_status_okay(self))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(self, compat_data)->ocd_data)
 		return (ENXIO);
 
