f8a520fbeab8ff5c0571dec97b640233a32a4bdf
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:19 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/ti/am335x/am335x_dmtpps.c
+++ sys/arm/ti/am335x/am335x_dmtpps.c
@@ -384,9 +384,6 @@ dmtpps_probe(device_t dev)
 	char strbuf[64];
 	int tmr_num;
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
