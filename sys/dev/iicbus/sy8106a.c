91b496e10c8aa26f540786e5cb05f28126300f2e
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:12 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/iicbus/sy8106a.c
+++ sys/dev/iicbus/sy8106a.c
@@ -245,8 +245,6 @@ sy8106a_regdev_map(device_t dev, phandle_t xref, int ncells, pcell_t *cells,
 static int
 sy8106a_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
