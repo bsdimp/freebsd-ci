48cb3a93ff61b32723b2914bf33631f1a9eeee70
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:58 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/beri/virtio/virtio_block.c
+++ sys/dev/beri/virtio/virtio_block.c
@@ -485,9 +485,6 @@ static int
 beri_vtblk_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "sri-cambridge,beri-vtblk"))
 		return (ENXIO);
 
