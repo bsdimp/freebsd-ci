9b8b973acb5a684c7e1ec8c7ec13579f657817fa
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:42 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/freescale/imx/imx_iomux.c
+++ sys/arm/freescale/imx/imx_iomux.c
@@ -184,9 +184,6 @@ static int
 iomux_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
