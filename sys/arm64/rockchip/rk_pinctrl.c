c8dd719e70ba6b9620db9574dd51ebebcb4dfe17
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:49 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm64/rockchip/rk_pinctrl.c
+++ sys/arm64/rockchip/rk_pinctrl.c
@@ -1213,9 +1213,6 @@ static int
 rk_pinctrl_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
