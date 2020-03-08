baa96c870ac3382edb5a9477ea746eb75e292cff
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:10 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/nvidia/tegra_soctherm.c
+++ sys/arm/nvidia/tegra_soctherm.c
@@ -532,9 +532,6 @@ static int
 soctherm_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
