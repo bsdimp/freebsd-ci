68732e8acf79b930709fc64977d329892cf27e9d
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:19 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/ti/aintc.c
+++ sys/arm/ti/aintc.c
@@ -239,8 +239,6 @@ ti_aintc_pic_attach(struct ti_aintc_softc *sc)
 static int
 ti_aintc_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
