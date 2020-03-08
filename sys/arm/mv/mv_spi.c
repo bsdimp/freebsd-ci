8c61d4b2e147cfb4bd363a11472aa160cf11ed63
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:59 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/mv/mv_spi.c
+++ sys/arm/mv/mv_spi.c
@@ -108,8 +108,6 @@ static int
 mv_spi_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
