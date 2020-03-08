c0f81fd3fc26ba43cba8114ccef70c06aa31b955
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:28 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/broadcom/bcm2835/bcm2835_dma.c
+++ sys/arm/broadcom/bcm2835/bcm2835_dma.c
@@ -659,9 +659,6 @@ static int
 bcm_dma_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
