50058261ed6ab65e1930dc38f8e59802289b6fb3
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:56 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/axgbe/if_axgbe.c
+++ sys/dev/axgbe/if_axgbe.c
@@ -266,9 +266,6 @@ static int
 axgbe_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
