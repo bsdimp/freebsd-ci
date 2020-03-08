9a82666841a562c87b033495a03eec79a50fa7f9
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:46 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm64/rockchip/rk_grf.c
+++ sys/arm64/rockchip/rk_grf.c
@@ -55,8 +55,6 @@ static int
 rk_grf_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
