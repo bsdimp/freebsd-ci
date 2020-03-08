782c4e557934761f82397c4636a775852e7b16b0
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:17 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/samsung/exynos/exynos5_pmu.c
+++ sys/arm/samsung/exynos/exynos5_pmu.c
@@ -89,9 +89,6 @@ static int
 pmu_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data != 0) {
 		device_set_desc(dev, "Samsung Exynos 5 Power Management Unit");
 		return (BUS_PROBE_DEFAULT);
