2ce61bc050e776f1c84614805c47d1d92b633ea9
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:29 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/usb/controller/generic_xhci_fdt.c
+++ sys/dev/usb/controller/generic_xhci_fdt.c
@@ -71,9 +71,6 @@ static int
 generic_xhci_fdt_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
