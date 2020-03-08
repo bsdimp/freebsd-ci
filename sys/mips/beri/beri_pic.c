c0d81125952d112dd6f08e05d63343994b4317ca
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:35 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/beri/beri_pic.c
+++ sys/mips/beri/beri_pic.c
@@ -156,9 +156,6 @@ static int
 beripic_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "sri-cambridge,beri-pic"))
 		return (ENXIO);
 		
