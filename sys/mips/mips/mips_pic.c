75b8a9aa14deffab5d956ac4e22e7dc61ee3a31c
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:52 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/mips/mips_pic.c
+++ sys/mips/mips/mips_pic.c
@@ -148,9 +148,6 @@ mips_pic_probe(device_t dev)
 {
 
 #ifdef FDT
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 #endif
