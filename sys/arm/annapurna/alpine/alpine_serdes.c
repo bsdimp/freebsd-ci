7ec3c59a6edbfd38b4b9a110b75b0157aa3ba22f
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:24 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/annapurna/alpine/alpine_serdes.c
+++ sys/arm/annapurna/alpine/alpine_serdes.c
@@ -93,9 +93,6 @@ static int
 al_serdes_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "annapurna-labs,al-serdes"))
 		return (ENXIO);
 
