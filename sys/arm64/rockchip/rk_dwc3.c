af470ae3229f37256ecbc725fc926ff9f6d38d02
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:57:45 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm64/rockchip/rk_dwc3.c
+++ sys/arm64/rockchip/rk_dwc3.c
@@ -83,9 +83,6 @@ rk_dwc3_probe(device_t dev)
 {
 	phandle_t node;
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
