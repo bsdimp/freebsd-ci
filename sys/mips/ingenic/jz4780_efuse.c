c57b38a13bee48222fc1ec71a2e7ec32b7d71e79
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:38 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/ingenic/jz4780_efuse.c
+++ sys/mips/ingenic/jz4780_efuse.c
@@ -80,8 +80,6 @@ struct jz4780_efuse_softc {
 static int
 jz4780_efuse_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
