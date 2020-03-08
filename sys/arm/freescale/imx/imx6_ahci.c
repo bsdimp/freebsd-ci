a3e3004e4f370320bca31a1d6c8c3f39c0560781
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:36 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/freescale/imx/imx6_ahci.c
+++ sys/arm/freescale/imx/imx6_ahci.c
@@ -216,10 +216,6 @@ static int
 imx6_ahci_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev)) {
-		return (ENXIO);
-	}
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data) {
 		return (ENXIO);
 	}
