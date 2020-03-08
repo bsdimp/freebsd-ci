a9111a30e5914dd143fcfc0610a7eaa23457e467
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:56:23 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/arm/annapurna/alpine/alpine_pci.c
+++ sys/arm/annapurna/alpine/alpine_pci.c
@@ -91,9 +91,6 @@ static int
 al_pcib_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_search_compatible(dev, compat_data)->ocd_data)
 		return (ENXIO);
 
