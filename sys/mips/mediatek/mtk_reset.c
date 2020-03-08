a96b80b020803deff6274ccf2461997c998e05a0
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:49 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/mediatek/mtk_reset.c
+++ sys/mips/mediatek/mtk_reset.c
@@ -54,9 +54,6 @@ static int
 mtk_reset_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
