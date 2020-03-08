dcd978de00635f73aa419abfb48b95c57adf1324
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:36 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/mips/ingenic/jz4780_clock.c
+++ sys/mips/ingenic/jz4780_clock.c
@@ -503,9 +503,6 @@ static int
 jz4780_clock_probe(device_t dev)
 {
 
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
-
 	if (!ofw_bus_is_compatible(dev, "ingenic,jz4780-cgu"))
 		return (ENXIO);
 
