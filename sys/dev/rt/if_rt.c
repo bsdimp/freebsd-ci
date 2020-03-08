280fed4169fb8d57fe55f148d298a9a19dedacb3
M. Warner Losh <wlosh@netflix.com>
2020-03-08 14:58:20 -0600

    Remove ofw_bus_status_okay test that was hoisted into parent bus

--- sys/dev/rt/if_rt.c
+++ sys/dev/rt/if_rt.c
@@ -2923,8 +2923,6 @@ DRIVER_MODULE(mdio, rtmdio, mdio_driver, mdio_devclass, 0, 0);
 static int
 rtmdio_probe(device_t dev)
 {
-	if (!ofw_bus_status_okay(dev))
-		return (ENXIO);
 
 	if (!ofw_bus_is_compatible(dev, "ralink,rt2880-mdio"))
 		return (ENXIO);
